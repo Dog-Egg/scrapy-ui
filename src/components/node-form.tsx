add732924f4cfc0d588a1375436c2b28a04cf208
Lee <294622946@qq.com>
2024-01-31 10:21:15 +0800

    add: test connection

--- src/components/node-form.tsx
+++ src/components/node-form.tsx
@@ -11,7 +11,16 @@ import {
   FormMessage,
 } from "./ui/form";
 import Input from "./Input";
-import { forwardRef, useImperativeHandle, useRef } from "react";
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
+import Button from "./shorts/button";
+import { daemonstatus } from "@/actions";
+import { CheckIcon, Cross2Icon } from "@radix-ui/react-icons";
 
 const formSchema = z.object({
   url: z.string({ required_error: "URL is required." }).url(),
@@ -51,6 +60,19 @@ export const NodeForm = forwardRef<NodeFormHandle, NodeFormProps>(function (
     [setError],
   );
 
+  // test connection
+  const watchUrl = form.watch("url");
+  const [testLoading, setTestLoading] = useState(false);
+  const [testResult, setTestResult] = useState<"success" | "failure" | "none">(
+    "none",
+  );
+  useEffect(() => {
+    // url 改变后清除测试结果
+    setTestResult(() => {
+      return "none";
+    });
+  }, [watchUrl]);
+
   return (
     <Form {...form}>
       <form
@@ -71,8 +93,58 @@ export const NodeForm = forwardRef<NodeFormHandle, NodeFormProps>(function (
           )}
         />
       </form>
+      {isUrl(watchUrl) && (
+        <div className="flex items-center">
+          <Button
+            loading={testLoading}
+            variant="outline"
+            size="sm"
+            onClick={async () => {
+              setTestLoading(true);
+              setTestResult("none");
+              try {
+                await daemonstatus(watchUrl);
+                setTestResult("success");
+              } catch {
+                setTestResult("failure");
+              } finally {
+                setTestLoading(false);
+              }
+            }}
+          >
+            Test Connection
+          </Button>
+          <span
+            className={
+              "ml-2 inline-flex items-center text-xs text-muted-foreground"
+            }
+          >
+            {testResult == "success" && (
+              <>
+                Connection successful
+                <CheckIcon className="text-success ml-1 h-4 w-4" />
+              </>
+            )}
+            {testResult == "failure" && (
+              <>
+                Connection failure
+                <Cross2Icon className="ml-1 h-4 w-4 text-destructive" />
+              </>
+            )}
+          </span>
+        </div>
+      )}
     </Form>
   );
 });
 
 NodeForm.displayName = "NodeForm";
+
+function isUrl(value: string) {
+  try {
+    new URL(value);
+    return true;
+  } catch {
+    return false;
+  }
+}
